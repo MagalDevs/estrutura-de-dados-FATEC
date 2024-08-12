8be5491efa1c08a6bf98cc39a1cbedb6b7ac820e
magaldevs <1051392411001@fatec>
2024-08-12 10:56:05 -0300

    adicionando informações da aula

--- aula-12-08-24/aula1.js
+++ aula-12-08-24/aula1.js
@@ -1,18 +1,59 @@
-/*
+
 console.log("caió lindo");
 alert("kn gostoso e gustavin lindo");
-var num = 1;
-num = 3;
-var price = 1.5;
-const myname = "Magas";
-var nullvar = null;
-var und = undefined;
-console.log("num: " + num);
-console.log("Meu nome: " + myname);
-console.log("preço: " + price);
-console.log("nullVar: " + nullvar);
-console.log("und: " + und);
-*/
+var num = 1; // {1}
+num = 3; // {2}
+var price = 1.5; // {3}
+var myName = 'Packt'; // {4}
+var trueValue = true; // {5}
+var nullVar = null; // {6}
+var und; // {7}
+
+console.log('num: ' + num);
+console.log('myName: ' + myName);
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);
+
+var myVariable = 'global';
+var myOtherVariable = 'global';
+function myFunction() {
+var myVariable = 'local';
+return myVariable;
+}
+function myOtherFunction() {
+myOtherVariable = 'local';
+return myOtherVariable;
+}
+console.log(myVariable); //{1}
+console.log(myFunction()); //{2}
+console.log(myOtherVariable); //{3}
+console.log(myOtherFunction()); //{4}
+console.log(myOtherVariable); //{5}
+
+var num = 0; // {1}
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+num += 1; // {2}
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+console.log('num == 1 : ' + (num == 1)); // {3}
+console.log('num === 1 : ' + (num === 1));
+console.log('num != 1 : ' + (num != 1));
+console.log('num > 1 : ' + (num > 1));
+console.log('num < 1 : ' + (num < 1));
+console.log('num >= 1 : ' + (num >= 1));
+console.log('num <= 1 : ' + (num <= 1));
+console.log('true && false : ' + (true && false)); // {4}
+console.log('true || false : ' + (true || false));
+console.log('!true : ' + (!true));
+/*
 function soma(){
     let soma = 0;
     let num1 = parseInt(prompt('digite um número'));
@@ -39,3 +80,4 @@ function soma(){
 }
 
 soma();
+*/
