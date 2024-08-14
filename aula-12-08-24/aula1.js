834d4356cb8a1924ffdd47168606764107f5b170
magaldevs <danielgalfla12@gmail.com>
2024-08-14 07:57:08 -0300

    finalizando a atividade da ultima aula

--- aula-12-08-24/aula1.js
+++ aula-12-08-24/aula1.js
@@ -1,6 +1,6 @@
 
-console.log("caió lindo");
-alert("kn gostoso e gustavin lindo");
+console.log("Hello World!");
+alert("Hello World!");
 var num = 1; // {1}
 num = 3; // {2}
 var price = 1.5; // {3}
@@ -53,31 +53,3 @@ console.log('num <= 1 : ' + (num <= 1));
 console.log('true && false : ' + (true && false)); // {4}
 console.log('true || false : ' + (true || false));
 console.log('!true : ' + (!true));
-/*
-function soma(){
-    let soma = 0;
-    let num1 = parseInt(prompt('digite um número'));
-    soma += num1
-    while(true){
-        console.log(soma)
-        let num2 = parseInt(prompt('escolha um número pra somar(digite 0 para parar)'));
-        if (num2 == 0){
-            alert(soma);
-            break;
-        } else{
-            soma += num2;
-            continue;
-        }
-    }
-
-    if(soma >= 100){
-        document.write(` O valor de ${soma} da variável soma, é maior que 100`)
-    } else {
-        document.write(` O valor de ${soma} da variável soma, é menor que 100`)
-    }
-
-    return soma
-}
-
-soma();
-*/
